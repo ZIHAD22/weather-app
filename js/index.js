1d24a6fa2e8def3a9763ec8005c754b119e02dbf
ZIHAD22 <mdzihad2244@gmail.com>
2022-03-04 00:15:13 +0600

    add some validation and spinner

--- js/index.js
+++ js/index.js
@@ -1,38 +1,61 @@
 const API_KEY = `e0960319f367343c6ceca2d747f36b8c`
 
-// featcing function
+// fetching function
 const fetching = async (url) => {
   const data = await fetch(url)
   const result = await data.json()
   return result
 }
 
+// searching function
 const searchResult = async () => {
+  toggle('spinner', 'block', 'd-none')
+  toggle('weather', 'none')
   const city = document.getElementById('city').value
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
   const searchingResult = await fetching(url)
   displayData(searchingResult)
+  toggle('spinner', 'none')
+  toggle('weather', 'block')
 }
 
-const displayData = (data) => {
-  console.log(data)
-  const displayWeather = document.getElementById('weather')
-  displayWeather.textContent = ''
-  const div = document.createElement('div')
-  const {
-    name,
-    main: { temp },
-  } = data
-  const main = data.weather.map((e) => e.main)
-  const icon = data.weather.map((e) => e.icon)
-  console.log(main, icon)
-  div.innerHTML = `
-        <img width="100px" src="https://openweathermap.org/img/wn/${icon}.png" alt="" />
-        <h1>${name}</h1>
-        <h3><span>${(temp - 273.15).toFixed(2)}</span>&deg;C</h3>
-        <h1 class="lead">${main}</h1>
-  
-  `
+// toggle spinner function
+const toggle = (id, style, isRemoved) => {
+  const object = document.getElementById(id)
+  object.style.display = style
+  isRemoved ? object.classList.remove(isRemoved) : ''
+}
 
-  displayWeather.appendChild(div)
+// display function
+const displayData = (data) => {
+  if (data.cod == 404) {
+    const displayWeather = document.getElementById('weather')
+    displayWeather.textContent = ''
+    const div = document.createElement('div')
+    div.innerHTML = `
+    
+        <h2>${data.message}</h2>
+    
+    `
+    displayWeather.appendChild(div)
+  } else {
+    const displayWeather = document.getElementById('weather')
+    displayWeather.textContent = ''
+    const div = document.createElement('div')
+    const {
+      name,
+      main: { temp },
+    } = data
+    const main = data.weather.map((e) => e.main)
+    const icon = data.weather.map((e) => e.icon)
+    div.innerHTML = `
+              <img width="100px" src="https://openweathermap.org/img/wn/${icon}.png" alt="" />
+              <h1>${name}</h1>
+              <h3><span>${(temp - 273.15).toFixed(2)}</span>&deg;C</h3>
+              <h1 class="lead">${main}</h1>
+        
+        `
+
+    displayWeather.appendChild(div)
+  }
 }
